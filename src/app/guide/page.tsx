deb19601a5bd08b8c6f2972ef38042fcf08bc67e
cyrax1800 <mchen0358@gmail.com>
2024-11-09 22:23:09 +0700

    update guide

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation'
 
 export default async function Guide() {
-    redirect("https://drive.google.com/drive/folders/1XTHuSyBeU5l6zcGY25LD5o0tcFgKUs1h?usp=sharing")
+    redirect("https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTEzNTcxMzEzNTc0NzEx?igsh=MWcwcXJxY2I1ZHY3ZQ==")
 }
